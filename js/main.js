db937c8acdf166f2a37df6d861ad55cbe2296790
Медведев Борис <bmedved78@mail.ru>
2022-09-05 20:20:32 +0300

    888

--- js/main.js
+++ js/main.js
@@ -1,22 +1,43 @@
+const videoPlay = document.querySelector('.video-controller--play');
+const videoPause = document.querySelector('.video-controller--pause');
+const video = document.querySelector('.video');
+const barLine = document.querySelector('.bar__line');
+videoPlay.addEventListener('click', () => {
+  video.play();
+  videoPlay.style.display = 'none';
+  videoPause.style.display = 'block';
+});
+videoPause.addEventListener('click', () => {
+  video.pause();
+  videoPause.style.display = 'none';
+  videoPlay.style.display = 'block';
+});
+
+video.ontimeupdate = function () {
+  let persentage = (video.currentTime / video.duration) * 100;
+  barLine.style.width = persentage + '%';
+};
+
 document.addEventListener('DOMContentLoaded', () => {
-
   const burger = document.querySelector('.burger');
+  const menu = document.querySelector('.menu');
+  let keys = {
+    ESC: 27,
+  };
 
-  burger.addEventListener('click', () => {
-    burger.classList.toggle('burger--active');
-  });
-
+  function showMenu() {
+    menu.classList.add('menu--showed');
+  };
+  burger.addEventListener('click', showMenu);
 });
 
 
 
-const root = document.documentElement;
-const darkBackgraund = '#2b2b2b';
-const lightBackgraund = '#fafafa';
+// const root = document.documentElement;
+// const darkBackgraund = '#2b2b2b';
+// const lightBackgraund = '#fafafa';
+// const button = document.querySelector('.header__burger');
+// button.addEventListener('click', () => {
+//   root.style.setProperty('--team-backgraund', lightBackgraund);
 
-const button = document.querySelector('.header__burger');
-
-button.addEventListener('click', () => {
-  root.style.setProperty('--team-backgraund', lightBackgraund);
-
-});
+// });
